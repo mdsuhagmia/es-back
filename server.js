738dc6dba19aac1d168df908670d9c1fc250f556
Md. Suhag Mia <mdshohagmia53200@gmail.com>
2025-12-31 12:35:22 +0600

    server js directory change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-const connectDatabase = require("./config/db");
-const { serverPort } = require("./secret");
+const app = require("./src/app");
+const connectDatabase = require("./src/config/db");
+const { serverPort } = require("./src/secret");
 
 app.listen(serverPort, async ()=>{
     console.log(`Server is running at http://localhost:${serverPort}`);
