c389950574deacfd3f99ec0180ce360e0e20c6ba
Md. Suhag Mia <mdshohagmia53200@gmail.com>
2026-01-02 12:51:25 +0600

    iiii

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { handleLogin, handleLogout, handleRefreshToken, handleProtected } = require('../controller/authController');
-const { isLoggedOut, isLoggedIn } = require('../middlewares/auth');
+const { isLoggedOut, isLoggedIn, isAdmin } = require('../middlewares/auth');
 const { validateUserLogin } = require('../validator/auth');
 const runValidation = require('../validator');
 const authRouter = express.Router();
@@ -9,7 +9,7 @@ authRouter.post("/login", validateUserLogin, runValidation, isLoggedOut, handleL
 authRouter.post("/logout", isLoggedIn, handleLogout);
 authRouter.get("/refresh-token", handleRefreshToken);
 authRouter.get("/protected", handleProtected);
-authRouter.get("/me", isLoggedIn, (req, res) => {
+authRouter.get("/me", isLoggedIn, isAdmin, (req, res) => {
   res.status(200).json({ user: req.user });
 });
 
